727e8205bb409d23fb42925e3b4b27f6ec03dddc
Yuliia Popelniak <popelniak96@gmail.com>
2024-05-08 15:36:28 -0500

    ORM-Queries Part2

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.cydeo.boostrap;
 
+import com.cydeo.repository.CourseRepository;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
@@ -15,11 +16,13 @@ public class DataGenerator implements CommandLineRunner {
     private final RegionRepository RegionRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository RegionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+    public DataGenerator(RegionRepository RegionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.RegionRepository = RegionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -54,20 +57,41 @@ public class DataGenerator implements CommandLineRunner {
 
         System.out.println("findByFirstNameAndLastNameOrEmail: " + employeeRepository.findByFirstNameAndLastNameOrEmail("Sydney","Symonds","vmarwickm@upenn.edu"));
 
-        System.out.println(employeeRepository.findByFirstNameIsNot("Sydney"));
+//        System.out.println("findByFirstNameIsNot" + employeeRepository.findByFirstNameIsNot("Sydney"));
 
-        System.out.println(employeeRepository.findByFirstNameStartingWith("Syd"));
+        System.out.println("findByFirstNameStartingWith" + employeeRepository.findByFirstNameStartingWith("Syd"));
+//
+//        System.out.println("findBySalaryGreaterThan" + employeeRepository.findBySalaryGreaterThan(10000));
 
-        System.out.println(employeeRepository.findBySalaryGreaterThan(2000));
-
-        System.out.println("findBySalaryLessThanEqual"  + employeeRepository.findBySalaryLessThanEqual(4000));
-
-        System.out.println("findByHireDateBetween:" + employeeRepository.findByHireDateBetween(LocalDate.parse("2006-02-19"),LocalDate.parse("2014-06-23")));
-
-        System.out.println("getEmployeeDetail" + employeeRepository.getEmployeeDetail());
+//        System.out.println("findBySalaryLessThanEqual"  + employeeRepository.findBySalaryLessThanEqual(4000));
+//
+//        System.out.println("findByHireDateBetween:" + employeeRepository.findByHireDateBetween(LocalDate.parse("2006-02-19"),LocalDate.parse("2014-06-23")));
+//
+//        System.out.println("getEmployeeDetail" + employeeRepository.getEmployeeDetail());
 
         System.out.println("getEmployeeSalary : " + employeeRepository.getEmployeeSalary());
 
         System.out.println("getEmployeeDetail" + employeeRepository.getEmployeeDetail("vmarwickm@upenn.edu"));
+
+        System.out.println("******Course********");
+
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+
+        System.out.println("******^^^^^^********");
+
+        courseRepository.findByCategoryOrderByName("Spring").forEach(System.out::println);
+
+        System.out.println("******^^^^^^********");
+
+        System.out.println(courseRepository.existsByName("Java"));
+        System.out.println("******^^^^^^********");
+
+        System.out.println(courseRepository.countByCategory("Spring"));
+        System.out.println("******^^^^^^********");
+        courseRepository.findByNameStartingWith("Rapid").forEach(System.out::println);
+        System.out.println("******^^^^^^********");
+//        courseRepository.streamByCategory("Spring").forEach(System.out::println);
+
+
     }
 }
