12f901be935fb211b375b17e0cb362d7375967be
Yuliia Popelniak <popelniak96@gmail.com>
2024-05-09 16:01:31 -0500

    ORM-Queries Part2

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -20,10 +22,14 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     //returns the count of courses for the provided category
     int countByCategory(String category);
 
+    //find all courses that start with the provided course name
     List<Course> findByNameStartingWith(String name);
 
     //find all courses by category and returns a stream
     Stream<Course> streamByCategory(String category);
+
+    @Query("SELECT c FROM Course c WHERE c.category = :category AND c.rating > :rating")
+    List<Course> findAllByCategoryAndRatingGreaterThan(@Param("category") String category, @Param("rating") int rating);
 
 
 
--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,45 +3,48 @@ package com.cydeo.repository;
 import com.cydeo.entity.Employee;
 import lombok.Lombok;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-   List<Employee> findByEmail(String email);
+    List<Employee> findByEmail(String email);
 
-   List<Employee> findByFirstNameAndLastNameOrEmail(String firstName, String lastName, String email);
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName, String lastName, String email);
 
-   List<Employee> findByFirstNameIsNot(String firstName);
+    List<Employee> findByFirstNameIsNot(String firstName);
 
-   List<Employee> findByFirstNameStartingWith(String pattern);
+    List<Employee> findByFirstNameStartingWith(String pattern);
 
-   List<Employee> findBySalaryGreaterThan(Integer salary);
+    List<Employee> findBySalaryGreaterThan(Integer salary);
 
-   List<Employee> findBySalaryLessThanEqual(Integer salary);
+    List<Employee> findBySalaryLessThanEqual(Integer salary);
 
-   List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
+    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
 
-   List<Employee> findBySalaryGreaterThanEqualOrderBySalaryDesc(Integer salary);
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalaryDesc(Integer salary);
 
-   List<Employee> findDistinctTop3BySalaryLessThanEqual(Integer salary);
+    List<Employee> findDistinctTop3BySalaryLessThanEqual(Integer salary);
 
-   List<Employee> findByEmailIsNull();
+    List<Employee> findByEmailIsNull();
 
-    @Query("SELECT e FROM Employee e WHERE e.email = 'jgresly14@ted.com'")
+    @Query("SELECT e FROM Employee e WHERE e.email = 'sdubber7@t-online.de'")
     Employee getEmployeeDetail();
 
-    @Query( "SELECT e.salary FROM Employee e WHERE e.email = 'jgresly14@ted.com'")
+    @Query("SELECT e.salary FROM Employee e WHERE e.email = 'sdubber7@t-online.de'")
     Integer getEmployeeSalary();
 
     @Query("SELECT e FROM Employee e WHERE e.email=?1")
     Optional<Employee> getEmployeeDetail(String email);
 
     @Query("SELECT e FROM Employee e WHERE e.email=?1 AND e.salary=?2")
-    Employee getEmployeeDetail(String email,int salary);
+    Employee getEmployeeDetail(String email, int salary);
 
     //Not Equal
     @Query("SELECT e FROM Employee e WHERE e.salary <> ?1 ")
@@ -51,23 +54,29 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
     List<Employee> getEmployeeFirstNameLike(String pattern);
 
+    //less than
     @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
-    List<Employee> getEmployeesSalaryLessThan(Integer salary);
+    List<Employee> getEmployeeSalaryLessThan(int salary);
 
-    @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
-    List<Employee> getEmployeesSalaryGreaterThan(Integer salary);
+    //greater than
+    @Query("SELECT e FROM Employee e WHERE e.salary > ?1")
+    List<Employee> getEmployeeSalaryGreaterThan(int salary);
 
+    //Before
     @Query("SELECT e FROM Employee e WHERE e.hireDate > ?1")
-    List<Employee> getEmployeesHiredDateBefore(LocalDate date);
+    List<Employee> getEmployeeHireDateBefore(LocalDate date);
 
+    //Between
     @Query("SELECT e FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
-    List<Employee> getEmployeesSalaryBetween(int salary1,int salary2);
+    List<Employee> getEmployeeSalaryBetween(int salary1, int salary2);
 
+    //Null
     @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
-     List<Employee> getEmployeesEmailIsNull();
+    List<Employee> getEmployeeEmailIsNull();
 
+    //Not Null
     @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
-    List<Employee> getEmployeesEmailIsNotNull();
+    List<Employee> getEmployeeEmailIsNotNull();
 
     //Sorting in ascending order
     @Query("SELECT e FROM Employee e ORDER BY e.salary")
@@ -78,5 +87,20 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> getEmployeeSalaryOrderDesc();
 
 
+    @Query(value = "SELECT * FROM employees WHERE salary ?1", nativeQuery = true)
+    List<Employee> readEmployeeDetailBySalary(int salary);
+
+    @Query("select e from Employee e where e.salary = :salary")
+    List<Employee> getEmployeeSalary(@Param("salary") int salary);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Employee e SET e.email = 'admin@email.com' WHERE e.id=:id")
+    void updateEmployeeJPQL(@Param("id") int id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE employees SET email = 'admin@email.com' WHERE id=:id", nativeQuery = true)
+    void updateEmployeeNativeQuery(@Param("id") int id);
 
 }
