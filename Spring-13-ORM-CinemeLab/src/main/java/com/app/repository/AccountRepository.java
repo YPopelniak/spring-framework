322926a07a5514bc5d4f00688fe996d1122b1239
Yuliia Popelniak <popelniak96@gmail.com>
2024-05-15 19:49:04 -0500

    Completed AccountRepository starter

--- Spring-13-ORM-CinemeLab/src/main/java/com/app/repository/AccountRepository.java
+++ Spring-13-ORM-CinemeLab/src/main/java/com/app/repository/AccountRepository.java
@@ -3,6 +3,8 @@ package com.app.repository;
 import com.app.entity.Account;
 import com.app.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,40 +16,54 @@ public interface AccountRepository  extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
 
-    //Write a derived query to list all accounts with a specific country or state
+    // query to list all accounts with a specific country or state
     List<Account> findAllByCountryOrState(String country,String state);
-    //Write a derived query to list all accounts with age lower than or equal to a specific value
+    // query to list all accounts with age lower than or equal to a specific value
 
     List<Account> findAllByAgeLessThanEqual(Integer age);
-    //Write a derived query to list all accounts with a specific role
+    // query to list all accounts with a specific role
 
     List<Account> findAllByRole(UserRole role);
 
-    //Write a derived query to list all accounts between a range of ages
+    // query to list all accounts between a range of ages
 
     List<Account> findAllByAgeBetween(Integer ageStart, Integer ageEnd);
 
-    //Write a derived query to list all accounts where the beginning of the address contains the keyword
+    // query to list all accounts where the beginning of the address contains the keyword
     List<Account> findAllByAddressStartsWith(String address);
 
-    //Write a derived query to sort the list of accounts with age
+    //  query to sort the list of accounts with age
 
     List<Account> findAllByOrderByAgeDesc();
 
     // ------------------- JPQL QUERIES ------------------- //
 
-    //Write a JPQL query that returns all accounts
+    // JPQL query that returns all accounts
 
-    //Write a JPQL query to list all admin accounts
+    @Query("SELECT a FROM Account a")
+    List<Account> fetchAllAccounts();
 
-    //Write a JPQL query to sort all accounts with age
+    // JPQL query to list all admin accounts
+
+    @Query("SELECT a FROM Account a WHERE a.role = 'ADMIN'")
+    List<Account> fetchAdminAccount();
+
+    // JPQL query to sort all accounts with age
+
+    @Query("SELECT a FROM Account a ORDER BY a.age")
+    List<Account> fetchAllOrderByAge();
 
     // ------------------- Native QUERIES ------------------- //
 
-    //Write a native query to read all accounts with an age lower than a specific value
-
+    // native query to read all accounts with an age lower than a specific value
+    @Query(value= "SELECT * FROM account_details WHERE age < ?1", nativeQuery=true)
+    List<Account> fetchAllAgeLowerThan(Integer age);
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
-
-    //Write a native query to read all accounts with an age lower than a specific value
-
+    @Query(value = "SELECT * FROM account_details WHERE LOWER(name) LIKE LOWER(concat('%',?1,'%')) OR address ILIKE concat('%',?1,'%') " +
+            " OR country ILIKE concat('%',?1,'%') or state ILIKE concat('%',?1,'%') " +
+            "OR city ILIKE concat('%',?1,'%') ",nativeQuery = true)
+    List<Account> retrieveBySearchCriteria(String pattern);
+    // native query to read all accounts with an age lower than a specific value
+    @Query(value= "SELECT * FROM account_details WHERE age > :age", nativeQuery=true)
+    List<Account> retrieveAllBasedOnAgeHigher(@Param("age") Integer age);
 }
