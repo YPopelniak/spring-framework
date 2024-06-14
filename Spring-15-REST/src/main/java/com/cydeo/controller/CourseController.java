67d34b16ee98c56143644742956b384329122fca
Yuliia Popelniak <popelniak96@gmail.com>
2024-06-13 20:30:05 -0500

    Get, Post, Put, Delete endpoints created

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // @Controller + @ResponseBody
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v2")
 public class CourseController {
 
     private final CourseService courseService;
